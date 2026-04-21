6b8451ec265fbff25ea20dda83ccc8b22c2ea7f8
Aditya-Dey012 <aditya2002dey@gmail.com>
2026-04-21 17:41:42 +0530

    added song

--- app.py
+++ app.py
@@ -562,7 +562,7 @@ def show_celebration_page():
     st.markdown('<div class="page-celebration-bg"></div>', unsafe_allow_html=True)
 
     # ── AUTO-PLAY MUSIC ──────────────────────────────────────────────────────
-    music_file = Path("assets/birthday_song.mp3")
+    music_file = Path("assets\Tere_Bina.m4a")
     if music_file.exists():
         st.audio(str(music_file), autoplay=True, loop=False)
 
